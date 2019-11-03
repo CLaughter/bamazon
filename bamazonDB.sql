d2c38bddeac4c4c192865cb14e1f3088f8e50333
Chris Laughter <chrislaugh@gmail.com>
2019-11-03 15:29:19 -0700

    change price number length

--- bamazonDB.sql
+++ bamazonDB.sql
@@ -8,7 +8,7 @@ CREATE TABLE products(
   item_id INT unsigned NOT NULL AUTO_INCREMENT,
   product_name VARCHAR(50) NULL,
   department_name VARCHAR(20) NOT NULL,
-  price DECIMAL(5,2) NULL,
+  price DECIMAL(10,2) NULL,
   stock_quantity INT NOT NULL,
   PRIMARY KEY (item_id)
 );
