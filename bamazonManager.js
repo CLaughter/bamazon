3158b99815bcd52ae0c586ae23cafef346c2aa6f
Chris Laughter <chrislaugh@gmail.com>
2019-11-06 13:17:35 -0700

    work in progress

--- bamazonManager.js
+++ bamazonManager.js
@@ -1,7 +1,7 @@
 var inquirer = require('inquirer');
 var mysql = require('mysql');
+require("console.table");
 
-// Create mysql connection
 var connection = mysql.createConnection( {
   host: 'localhost',
   port: 3306,
@@ -11,44 +11,73 @@ var connection = mysql.createConnection( {
 });
 
   connection.connect(function(err) {
-    if(err) throw err;
-    // Display if connected or throw error
+    if(err) {
+    console.error("Error connecting: " + err.stack);
+  } else {
     console.log("Connected as id: " + connection.threadId); 
-    mgrSelect();
-  });
+    makeTable();
+  }
+});
 
-// Query user input and define previously called function
+function makeTable() {
+  connection.query("SELECT * FROM products", function(err,res) {
+    if(err) {
+    console.error("Error connecting: " + err.stack);       
+  }
+  mgrSelect();
+  });
+}
+
+// Query user input
 function mgrSelect() {
-var productsForSale = 'View Products for Sale';
-var lowInventory = 'View Low Inventory';
-var addInventory = 'Add to Inventory';
-var addNewProduct = 'Add New Product';
+  var productsForSale = 'View Products for Sale';
+  var lowInventory = 'View Low Inventory';
+  var addInventory = 'Add to Inventory';
+  var addNewProduct = 'Add New Product';
+  var quit = "Quit";
 
   inquirer.prompt (
-      {
-        type: "list",
-        name: "mgrChoice",
-        choices: [
-          'productsForSale',
-          'lowInventory',
-          'addInventory',
-          'addNewProduct'
-        ]
-      }
-    ) 
-      // Respond to mgr input 
-      .then(function(answer) {
-        // Loop through products list and display row data in a list
-        for (var i = 0; i < res.length; i++) {
-          
-          if(answer.choices === productsForSale) {
-            console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity);
-            console.log('-------------------------------------------------');
-          } else if(answer.choices === lowInventory && stock_quantity <=5) {
-            // console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity);
-            console.log('-------------------------------------------------');
-          }         
-        }
-      });
+    {
+      name: "mgrChoice",
+      type: "list",
+      message: "Make your selection",
+      choices: [ productsForSale, lowInventory, addInventory, addNewProduct, quit]
     }
-          
+  ) 
+  
+  // Respond to mgr input and call function
+  .then(function(answer) {
+      if(answer.mgrChoice === productsForSale) {
+        viewProducts();
+      } else if(answer.mgrChoice === lowInventory && stock_quantity <=5) {
+        viewLowInv();
+      } else if(answer.mgrChoice === addInventory) {
+        viewAddInv();
+      } else if(answer.mgrChoice === addNewProduct) {
+        addNewProduct();
+      } else {
+        process.exit();
+      }
+  });  
+}
+
+// Fetch date and display in table format
+function viewProducts() {
+  // Make table with all data
+  connection.query("SELECT * FROM products", function(err,res) {
+    if(err) {
+    console.error("Error connecting: " + err.stack);       
+  } else {
+    console.table(res); 
+  }
+  console.log("======================================================================");
+  return mgrSelect();
+  });
+} 
+  
+
+function quit() {
+  console.log("You have exited the program.");
+  process.exit();
+}
+
